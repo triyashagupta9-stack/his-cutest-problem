d31b9fba913f54948584353c056706474dc60bad
vaishnaviiii <triyashagupta9@gmail.com>
2026-02-14 15:12:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,25 +1,13 @@
 function sayYes() {
-  const song = document.getElementById("loveSong");
+  // Play music
+  var song = document.getElementById("loveSong");
   song.play();
 
+  // Go to second page effect
   document.body.innerHTML = `
-    <div style="
-      background: linear-gradient(135deg,#ff99cc,#ff4da6);
-      height:100vh;
-      display:flex;
-      flex-direction:column;
-      justify-content:center;
-      align-items:center;
-      color:white;
-      font-family:Arial;
-      text-align:center;
-    ">
-      <h1 style="font-size:50px;">Yeh Ishq Hai 💕</h1>
-      <p style="font-size:25px;">From today…</p>
-      <h2 style="font-size:35px;">cheenu ❤️ chikuuu</h2>
-      <p style="font-size:20px;">Forever starts now ✨</p>
-      <img src="cute-boy-cheek-pinching-his-girlfriend-happy-valentine-cartoon-character-illustration_56104-367.avif" 
-           style="width:250px; margin-top:20px; border-radius:15px;">
-    </div>
-  `;
-}
+  <div style="text-align:center; padding:50px;">
+    <h1 style="font-size:40px;">Sakshaat 💖</h1>
+    <p style="font-size:22px;">Cheenu loves Chikuuu 💕</p>
+    <h2 style="font-size:30px;">Will you officially be mine? 💗</h2>
+  </div>
+`;
