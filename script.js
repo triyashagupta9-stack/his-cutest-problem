edff5f5f48bc112e57274d87f82fb2fae5d25664
vaishnaviiii <triyashagupta9@gmail.com>
2026-02-14 15:51:43 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,8 @@
 function sayYes() {
+  const popup = document.getElementById("popup");
+  const song = document.getElementById("loveSong");
 
-  document.body.innerHTML = `
-    <div style="text-align:center; padding:40px;">
-
-      <p style="font-size:28px; font-weight:bold;">
-        Cheenu loves Chikuuuu 💕
-      </p>
-
-      <img src="cute-boy-cheek-pinching-his-girlfriend-happy-valentine-cartoon-character-illustration_56104-367.avif" 
-           style="width:280px; margin-top:20px; border-radius:20px; box-shadow:0px 10px 25px rgba(0,0,0,0.2);">
-
-      <br><br>
-
-      <audio src="./love.mp3" autoplay controls></audio>
-
-    </div>
-  `;
+  popup.classList.add("show");
+  song.play();
 }
+
